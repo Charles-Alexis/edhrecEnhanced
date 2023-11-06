2bee6f525889f6603a716b10e131b6e95d1dd7f9
Charles-Alexis <lavc2115@usherbrooke.ca>
2023-11-05 19:07:27 -0500

    made entire collection to test + hard code debug utf-8 error (will need to make a code for this)

--- edhrecEnhanced.py
+++ edhrecEnhanced.py
@@ -10,7 +10,7 @@ import xlrd
 import pandas as pd
 import time
 
-dataframe = pd.read_excel('collection/test_100.xlsx')
+dataframe = pd.read_excel('collection/collection.xlsx')
 name_list = list()
 card_data = list()
 
@@ -20,18 +20,26 @@ for name in dataframe['Card Name']:
 counter = 0
 nbr_of_card = len(name_list)
 
+print('Number of Cards to Fetch: '+ str(nbr_of_card))
+
 time_start = time.time()
 scryfall_base_url = 'https://api.scryfall.com/cards/named?exact='
 for card_name in name_list:
     response = requests.get(scryfall_base_url + card_name)
     counter += 1
+    
     if response.status_code == 200:
         card_data.append(response.json())
     if response.status_code != 200:
         print('Probleme with card: ' + card_name)
-    if counter%100 == 0:
-        print('Percent completed: ' + "{:.2f}".format(counter/nbr_of_card*100) +' in ' + "{:.2f}".format(time.time() - time_start))
- 
+        
+    if counter%250 == 0:
+        timing = time.time() - time_start
+        print('%: ' + "{:.2f}".format(counter/nbr_of_card*100) +' in ' + "{:.2f}".format(timing) + ' Fetched card: ' +str(counter) + ' Estimated to end: ' +str(((timing*nbr_of_card)/counter)-timing ))
+    #time.sleep(0.1)
+
+print('%: ' + "{:.2f}".format(counter/nbr_of_card*100) +' in ' + "{:.2f}".format(time.time() - time_start) + ' Fetched card: ' +str(counter))
+
 possible_commander = list()
 for data in card_data:
     if 'Legendary Creature' in data['type_line']:# and '//' not in data['type_line']:
@@ -77,6 +85,18 @@ planeswalkers = list()
 for data in card_data:
     if 'Planeswalker' in data['type_line']:
         planeswalkers.append([data['name'],data])
+
+### PRINTING STATS        
+print('COLLECTION STATS')
+print('Possible commanders: '+str(len(possible_commander)))
+print('Creatures: '+str(len(creatures)))
+print('Instants: '+str(len(instants)))
+print('Sorceries: '+str(len(sorceries)))
+print('Artifacts: '+str(len(artifacts)))
+print('Enchantments: '+str(len(enchantments)))
+print('Planeswalkers: '+str(len(planeswalkers)))
+print('Battles: '+str(len(battles)))
+print('Lands: '+str(len(lands)))
 
 ### CHECKING SYNERGIES
 base_url = 'https://json.edhrec.com/pages/'
